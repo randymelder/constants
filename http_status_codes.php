98e03009b6ddbda51b8402912f9554da0f7e7c06
Randy Melder <randy@voloforce.com>
2020-09-12 06:24:41 -0700

    Adding PHP lib to supply HTTP status code info

---
+++ http_status_codes.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace HTTP;
+class ResponseCode {
+    const HTTP_STATUS_CODE_100_CONTINUE = [
+      "code" =>   100,
+      "description" => "CONTINUE"
+    ];
+    const HTTP_STATUS_CODE_101_SWITCHING_PROTOCOLS = [
+      "code" =>   101,
+      "description" => "SWITCHING PROTOCOLS"
+    ];
+    const HTTP_STATUS_CODE_102_PROCESSING = [
+      "code" =>   102,
+      "description" => "PROCESSING"
+    ];
+    const HTTP_STATUS_CODE_200_OK = [
+      "code" =>   200,
+      "description" => "OK"
+    ];
+    const HTTP_STATUS_CODE_201_CREATED = [
+      "code" =>   201,
+      "description" => "CREATED"
+    ];
+    const HTTP_STATUS_CODE_202_ACCEPTED = [
+      "code" =>   202,
+      "description" => "ACCEPTED"
+    ];
+    const HTTP_STATUS_CODE_203_NON_AUTHORITATIVE_INFORMATION = [
+      "code" =>   203,
+      "description" => "NON-AUTHORITATIVE INFORMATION"
+    ];
+    const HTTP_STATUS_CODE_204_NO_CONTENT = [
+      "code" =>   204,
+      "description" => "NO CONTENT"
+    ];
+    const HTTP_STATUS_CODE_205_RESET_CONTENT = [
+      "code" =>   205,
+      "description" => "RESET CONTENT"
+    ];
+    const HTTP_STATUS_CODE_206_PARTIAL_CONTENT = [
+      "code" =>   206,
+      "description" => "PARTIAL CONTENT"
+    ];
+    const HTTP_STATUS_CODE_207_MULTI_STATUS = [
+      "code" =>   207,
+      "description" => "MULTI-STATUS"
+    ];
+    const HTTP_STATUS_CODE_208_ALREADY_REPORTED = [
+      "code" =>   208,
+      "description" => "ALREADY REPORTED"
+    ];
+    const HTTP_STATUS_CODE_226_IM_USED = [
+      "code" =>   226,
+      "description" => "IM USED"
+    ];
+    const HTTP_STATUS_CODE_300_MULTIPLE_CHOICES = [
+      "code" =>   300,
+      "description" => "MULTIPLE CHOICES"
+    ];
+    const HTTP_STATUS_CODE_301_MOVED_PERMANENTLY = [
+      "code" =>   301,
+      "description" => "MOVED PERMANENTLY"
+    ];
+    const HTTP_STATUS_CODE_302_FOUND = [
+      "code" =>   302,
+      "description" => "FOUND"
+    ];
+    const HTTP_STATUS_CODE_303_SEE_OTHER = [
+      "code" =>   303,
+      "description" => "SEE OTHER"
+    ];
+    const HTTP_STATUS_CODE_304_NOT_MODIFIED = [
+      "code" =>   304,
+      "description" => "NOT MODIFIED"
+    ];
+    const HTTP_STATUS_CODE_305_USE_PROXY = [
+      "code" =>   305,
+      "description" => "USE PROXY"
+    ];
+    const HTTP_STATUS_CODE_306_ = [
+      "code" =>   306,
+      "description" => "(UNUSED)"
+    ];
+    const HTTP_STATUS_CODE_307_TEMPORARY_REDIRECT = [
+      "code" =>   307,
+      "description" => "TEMPORARY REDIRECT"
+    ];
+    const HTTP_STATUS_CODE_308_PERMANENT_REDIRECT = [
+      "code" =>   308,
+      "description" => "PERMANENT REDIRECT"
+    ];
+    const HTTP_STATUS_CODE_400_BAD_REQUEST = [
+      "code" =>   400,
+      "description" => "BAD REQUEST"
+    ];
+    const HTTP_STATUS_CODE_401_UNAUTHORIZED = [
+      "code" =>   401,
+      "description" => "UNAUTHORIZED"
+    ];
+    const HTTP_STATUS_CODE_402_PAYMENT_REQUIRED = [
+      "code" =>   402,
+      "description" => "PAYMENT REQUIRED"
+    ];
+    const HTTP_STATUS_CODE_403_FORBIDDEN = [
+      "code" =>   403,
+      "description" => "FORBIDDEN"
+    ];
+    const HTTP_STATUS_CODE_404_NOT_FOUND = [
+      "code" =>   404,
+      "description" => "NOT FOUND"
+    ];
+    const HTTP_STATUS_CODE_405_METHOD_NOT_ALLOWED = [
+      "code" =>   405,
+      "description" => "METHOD NOT ALLOWED"
+    ];
+    const HTTP_STATUS_CODE_406_NOT_ACCEPTABLE = [
+      "code" =>   406,
+      "description" => "NOT ACCEPTABLE"
+    ];
+    const HTTP_STATUS_CODE_407_PROXY_AUTHENTICATION_REQUIRED = [
+      "code" =>   407,
+      "description" => "PROXY AUTHENTICATION REQUIRED"
+    ];
+    const HTTP_STATUS_CODE_408_REQUEST_TIMEOUT = [
+      "code" =>   408,
+      "description" => "REQUEST TIMEOUT"
+    ];
+    const HTTP_STATUS_CODE_409_CONFLICT = [
+      "code" =>   408,
+      "description" => "CONFLICT"
+    ];
+    const HTTP_STATUS_CODE_410_GONE = [
+      "code" =>   410,
+      "description" => "GONE"
+    ];
+    const HTTP_STATUS_CODE_411_LENGTH_REQUIRED = [
+      "code" =>   411,
+      "description" => "LENGTH REQUIRED"
+    ];
+    const HTTP_STATUS_CODE_412_PRECONDITION_FAILED = [
+      "code" =>   412,
+      "description" => "PRECONDITION FAILED"
+    ];
+    const HTTP_STATUS_CODE_413_PAYLOAD_TOO_LARGE = [
+      "code" =>   413,
+      "description" => "PAYLOAD TOO LARGE"
+    ];
+    const HTTP_STATUS_CODE_414_URI_TOO_LONG = [
+      "code" =>   414,
+      "description" => "URI TOO LONG"
+    ];
+    const HTTP_STATUS_CODE_415_UNSUPPORTED_MEDIA_TYPE = [
+      "code" =>   415,
+      "description" => "UNSUPPORTED MEDIA TYP"
+    ];
+    const HTTP_STATUS_CODE_416_RANGE_NOT_SATISFIABLE = [
+      "code" =>   416,
+      "description" => "RANGE NOT SATISFIABLE"
+    ];
+    const HTTP_STATUS_CODE_417_EXPECTATION_FAILED = [
+      "code" =>   417,
+      "description" => "EXPECTATION FAILED"
+    ];
+    const HTTP_STATUS_CODE_421_MISDIRECTED_REQUEST = [
+      "code" =>   421,
+      "description" => "MISDIRECTED REQUEST"
+    ];
+    const HTTP_STATUS_CODE_422_UNPROCESSABLE_ENTITY = [
+      "code" =>   422,
+      "description" => "UNPROCESSABLE ENTITY"
+    ];
+    const HTTP_STATUS_CODE_423_LOCKED = [
+      "code" =>   423,
+      "description" => "LOCKED"
+    ];
+    const HTTP_STATUS_CODE_424_FAILED_DEPENDENCY = [
+      "code" =>   424,
+      "description" => "FAILED DEPENDENCY"
+    ];
+    const HTTP_STATUS_CODE_425_TOO_EARLY = [
+      "code" =>   425,
+      "description" => "TOO EARLY"
+    ];
+    const HTTP_STATUS_CODE_426_UPGRADE_REQUIRED = [
+      "code" =>   426,
+      "description" => "UPGRADE REQUIRED"
+    ];
+    const HTTP_STATUS_CODE_428_PRECONDITION_REQUIRED = [
+      "code" =>   428,
+      "description" => "PRECONDITION REQUIRED"
+    ];
+    const HTTP_STATUS_CODE_429_TOO_MANY_REQUESTS = [
+      "code" =>   429,
+      "description" => "TOO MANY REQUEST"
+    ];
+    const HTTP_STATUS_CODE_431_REQUEST_HEADER_FIELDS_TOO_LARGE = [
+      "code" =>   431,
+      "description" => "REQUEST HEADER FIELDS TOO LARGE"
+    ];
+    const HTTP_STATUS_CODE_500_INTERNAL_SERVER_ERROR = [
+      "code" =>   500,
+      "description" => "INTERNAL SERVER ERROR"
+    ];
+    const HTTP_STATUS_CODE_501_NOT_IMPLEMENTED = [
+      "code" =>   501,
+      "description" => "NOT IMPLEMENTED"
+    ];
+    const HTTP_STATUS_CODE_502_BAD_GATEWAY = [
+      "code" =>   502,
+      "description" => "BAD GATEWAY"
+    ];
+    const HTTP_STATUS_CODE_503_SERVICE_UNAVAILABLE = [
+      "code" =>   503,
+      "description" => "SERVICE UNAVAILABLE"
+    ];
+    const HTTP_STATUS_CODE_504_GATEWAY_TIMEOUT = [
+      "code" =>   504,
+      "description" => "GATEWAY TIMEOUT"
+    ];
+    const HTTP_STATUS_CODE_505_HTTP_VERSION_NOT_SUPPORTED = [
+      "code" =>   505,
+      "description" => "HTTP VERSION NOT SUPPORTED"
+    ];
+    const HTTP_STATUS_CODE_506_VARIANT_ALSO_NEGOTIATES = [
+      "code" =>   506,
+      "description" => "VARIANT ALSO NEGOTIATES"
+    ];
+    const HTTP_STATUS_CODE_507_INSUFFICIENT_STORAGE = [
+      "code" =>   507,
+      "description" => "INSUFFICIENT STORAGE"
+    ];
+    const HTTP_STATUS_CODE_508_LOOP_DETECTED = [
+      "code" =>   508,
+      "description" => "LOOP DETECTED"
+    ];
+    const HTTP_STATUS_CODE_510_NOT_EXTENDED = [
+      "code" =>   510,
+      "description" => "NOT EXTENDED"
+    ];
+    const HTTP_STATUS_CODE_511_NETWORK_AUTHENTICATION_REQUIRED = [
+      "code" =>   511,
+      "description" => "NETWORK AUTHENTICATION REQUIRED"
+    ];
+  }
